475fa64b354e4659a938994f68f4664f34da93db
hichamAchahbar <hichamwebdeveloper@gmail.com>
2022-10-31 14:38:18 +0100

    hide key

--- src/headerApi.js
+++ src/headerApi.js
@@ -1,5 +1,5 @@
 export const headers = {
-  'X-RapidAPI-Key': '52b3cfb1a8msh31d4efd0edfab14p10d67ajsn13a099e94e4a',
+  'X-RapidAPI-Key': process.env.REACT_APP_RADPID_API_KEY,
   'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
 }
 
